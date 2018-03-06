80ceb32ea97e36985046ecc455b064ad00b325aa
carlo <carlo@DESKTOP-8PNGQAN>
2018-03-06 00:19:28 -0600

    Creacion modulo carga de datos

---
+++ YellowBoxAutomatedTestTools/YellowBoxObjetos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YellowBoxAutomatedTestTools.WebDriverComponents
+{
+    class YellowBoxObjetos
+    {
+        public string Usuario { get; set; }
+        public string Contrasenia { get; set; }
+    }
+}
