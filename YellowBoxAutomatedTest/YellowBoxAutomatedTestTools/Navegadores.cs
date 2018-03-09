2b839040faa177b507e5db8f2dc23dbb70f0b4a6
User <User@DINN-GROUP>
2018-03-08 21:29:40 -0600

    clases nvas

---
+++ YellowBoxAutomatedTest/YellowBoxAutomatedTestTools/Navegadores.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Windows.Input;
+using System.Windows.Forms;
+using System.Drawing;
+using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UITest.Extension;
+using Keyboard = Microsoft.VisualStudio.TestTools.UITesting.Keyboard;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium;
+
+namespace YellowBoxAutomatedTestTools.WebDriversComponentes
+{
+    public class Navegadores
+    { 
+        public IWebDriver LanzarNavegador (string tipoNavegador)
+        {
+            IWebDriver driver = null;
+
+            if (tipoNavegador == "Chrome")
+            {
+                ChromeOptions opciones = new ChromeOptions();
+
+                opciones.AddArguments("-incognito");
+                opciones.AddArguments("--disble-infobars");
+                opciones.AddArguments("--disable-popup-bloking");
+                opciones.AddArguments("-start-maximized");
+            }
+            return driver;
+        }
+    }
+}
