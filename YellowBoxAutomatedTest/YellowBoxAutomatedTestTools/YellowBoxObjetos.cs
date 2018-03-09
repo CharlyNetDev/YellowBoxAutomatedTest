7f847ec418a2f8b488557ed61f417d77a35eb947
carlo <carlo@DESKTOP-8PNGQAN>
2018-03-08 21:56:46 -0600

    Nva propiedad

--- YellowBoxAutomatedTest/YellowBoxAutomatedTestTools/YellowBoxObjetos.cs
+++ YellowBoxAutomatedTest/YellowBoxAutomatedTestTools/YellowBoxObjetos.cs
@@ -10,5 +10,7 @@ namespace YellowBoxAutomatedTestTools.WebDriversComponentes
     {
         public string Usuario { get; set; }
         public string Password { get; set; }
+
+        public int MyProperty { get; set; }
     }
 }
