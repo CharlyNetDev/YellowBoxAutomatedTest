f6e3bea4f1bb3c7f2559f9f8882d18bd696bf13f
User <User@DINN-GROUP>
2018-03-08 23:08:12 -0600

    Cambios

--- YellowBoxAutomatedTest/YellowBoxAutomatedTestTools/YellowBoxObjetos.cs
+++ YellowBoxAutomatedTest/YellowBoxAutomatedTestTools/YellowBoxObjetos.cs
@@ -10,7 +10,5 @@ namespace YellowBoxAutomatedTestTools.WebDriversComponentes
     {
         public string Usuario { get; set; }
         public string Password { get; set; }
-
-        public int MyProperty { get; set; }
     }
 }
